79dc6a4da1cd7d41af705ae6ca5aa6ca9b1d33ba
AleksaPapovic <papovic.ra167.2018@uns.ac.rs>
2020-11-27 20:59:08 +0100

    #glavni_prozor Implementacija glavnog prozora i Singleton-a

---
+++ Documents/Oisis_projekat/Oisis_ssluzba/src/gui/MainFrame.java
@@ -0,0 +1,50 @@
+package gui;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7240939624608584060L;
+
+	private static MainFrame instance = null;
+	
+	private MainFrame() {
+		initialise();
+	}
+	
+	
+	private void initialise() {
+		
+		
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize();
+		int screenHeight = screenSize.height;
+		int screenWidth = screenSize.width;
+	
+		setSize(screenWidth / 2, screenHeight / 2);
+
+		setTitle("Studenska sluzba");
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setLocationRelativeTo(null);
+		
+		setVisible(true);	
+	
+	
+	}
+	
+	public static MainFrame getInstance() {
+		if(instance == null) {
+			instance = new MainFrame();
+		}
+		return instance;
+		}
+
+}
