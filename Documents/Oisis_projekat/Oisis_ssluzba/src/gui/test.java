562b11e1743ece5a9394b42a11f9ade4ccb222af
AleksaPapovic <papovic.ra167.2018@uns.ac.rs>
2020-11-27 20:59:08 +0100

    #glavni_prozor Implementacija glavnog prozora i Singleton-a

--- Documents/Oisis_projekat/Oisis_ssluzba/src/gui/test.java
+++ Documents/Oisis_projekat/Oisis_ssluzba/src/gui/test.java
@@ -1,9 +1,10 @@
+package gui;
 
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Test");
+			MainFrame mFrame = MainFrame.getInstance();
 	}
-
 }
