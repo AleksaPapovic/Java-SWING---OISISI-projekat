ecfd422fe1f83bff25027772f993c75d5e8c729c
dusanlekic22 <lekic.ra159.2018@uns.ac.rs>
2020-11-28 18:29:04 +0100

    #menu_bar Implementacija, ubacene itemi, ikonice i akceleratori

--- Documents/Oisis_projekat/Oisis_ssluzba/src/gui/Menubar.java
+++ Documents/Oisis_projekat/Oisis_ssluzba/src/gui/Menubar.java
@@ -1,20 +1,35 @@
-/*REFERENCA: https://alvinalexander.com/java/java-action-abstractaction-actionlistener/ */
-
+/*REFERENCA: https://alvinalexander.com/java/java-action-abstractaction-actionlistener/ 
+Icons made by "https://www.flaticon.com/authors/chanut" */
 package gui;
 
-import javax.swing.Action;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+import gui.Actions.AboutAction;
+import gui.Actions.CloseAction;
+import gui.Actions.DeleteAction;
+import gui.Actions.EditAction;
+import gui.Actions.HelpAction;
+import gui.Actions.NewAction;
 
 public class Menubar extends JMenuBar{
 	
-	Action newAction, closeAction, editAction, deleteAction, helpAction, aboutAction;
+	public  NewAction newAction;
+	public  CloseAction closeAction;
+	public  EditAction editAction;
+	public  DeleteAction deleteAction;
+	public  HelpAction helpAction;
+	public  AboutAction aboutAction;
 	
 	ImageIcon newIcon, closeIcon, editIcon, deleteIcon, helpIcon, aboutIcon;
-	
-	Actions ac = new Actions();
+
 	
 	public Menubar() {
 		super();
@@ -29,46 +44,60 @@ public class Menubar extends JMenuBar{
 	    // create our menu items, using the same actions the toolbar buttons use
 	    JMenuItem newMenuItem = new JMenuItem(newAction);
 	    JMenuItem closeMenuItem = new JMenuItem(closeAction);
+	    newMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+	    closeMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
+	    
+	    fileMenu.add(newMenuItem);
+	    fileMenu.addSeparator();
+	    fileMenu.add(closeMenuItem);
 	    
 	    JMenuItem editMenuItem = new JMenuItem(editAction);
 	    JMenuItem deleteMenuItem = new JMenuItem(deleteAction);
-	    
+	    editMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+		deleteMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, ActionEvent.CTRL_MASK));
+		
+	    editMenu.add(editMenuItem);
+	    editMenu.addSeparator();
+	    editMenu.add(deleteMenuItem);
+	      
 	    JMenuItem helpMenuItem = new JMenuItem(helpAction);
 	    JMenuItem aboutMenuItem = new JMenuItem(aboutAction);
-	    
-	    fileMenu.add(newMenuItem);
-	    fileMenu.add(closeMenuItem);
-	   
-	    editMenu.add(editMenuItem);
-	    editMenu.add(deleteMenuItem);
+	    helpMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, ActionEvent.CTRL_MASK));
+		aboutMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
 	    
 	    helpMenu.add(helpMenuItem);
+	    helpMenu.addSeparator();
 	    helpMenu.add(aboutMenuItem);
-
-	    // add the menus to the menubar
+	    
+	    add(Box.createHorizontalStrut(15));
 	    add(fileMenu);
+	    add(Box.createHorizontalStrut(10));
 	    add(editMenu);
+	    add(Box.createHorizontalStrut(10));
 	    add(helpMenu);
 	    
 	}
 	
 	private void createActions()
 	  {
+		
+		Actions ac = new Actions();
 	    // create our icons (for the toolbar)
-	    //newIcon = new ImageIcon(Menubar.class.getResource("new-32.png"));
-	    //editIcon = new ImageIcon(Menubar.class.getResource("edit-32.png"));
-	    //deleteIcon = new ImageIcon(Menubar.class.getResource("delete-32.png"));
+	    newIcon = new ImageIcon("icons/plus.png");
+	    closeIcon = new ImageIcon("icons/close.png");
+	    editIcon = new ImageIcon("icons/pen.png");
+	    deleteIcon = new ImageIcon("icons/trashbin.png");
+	    helpIcon = new ImageIcon("icons/help.png");
+	    aboutIcon = new ImageIcon("icons/about.png");
 
 	    // create our actions
-	    //NewAction newAction = ac.new NewAction("New", newIcon, "Create new", new Integer(KeyEvent.VK_N));
-	    //EditAction editAction = ac.new EditAction("Edit", editIcon, "Edit data", new Integer(KeyEvent.VK_E));
-	    //DeleteAction deleteAction = ac.new DeleteAction("Delete", deleteIcon, "Delete data", new Integer(KeyEvent.VK_D));
-	    
-	    
-	    
+	     newAction = ac.new NewAction("New", newIcon, "Create new", new Integer(KeyEvent.VK_N));
+	     closeAction = ac.new CloseAction("Close", closeIcon, "Close", new Integer(KeyEvent.VK_C));
+	     editAction = ac.new EditAction("Edit", editIcon, "Edit data", new Integer(KeyEvent.VK_E));
+	     deleteAction = ac.new DeleteAction("Delete", deleteIcon, "Delete data", new Integer(KeyEvent.VK_D));
+	     helpAction = ac.new HelpAction("Help", helpIcon, "Help", new Integer(KeyEvent.VK_H));
+	     aboutAction = ac.new AboutAction("About", aboutIcon, "Information", new Integer(KeyEvent.VK_A));
+	   
 	  }
-	 
 	
-	
-	 
 }	
