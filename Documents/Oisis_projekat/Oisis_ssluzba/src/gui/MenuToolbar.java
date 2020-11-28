2ce77965f4638b48702eac85f245e2344ea0dd14
AleksaPapovic <papovic.ra167.2018@uns.ac.rs>
2020-11-28 14:24:03 +0100

    #toolbar Implementacija searchbar-a i BoxLayout-a

--- Documents/Oisis_projekat/Oisis_ssluzba/src/gui/MenuToolbar.java
+++ Documents/Oisis_projekat/Oisis_ssluzba/src/gui/MenuToolbar.java
@@ -4,7 +4,11 @@ package gui;
 
 
 
+import java.awt.Dimension;
+
 import javax.swing.Action;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JTextField;
@@ -15,34 +19,66 @@ public class MenuToolbar  extends JToolBar{
 
 	
 	Action newAction, editAction, deleteAction;
+	
+	public static JTextField searchbar;
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	public MenuToolbar() {
 		
-	super(SwingConstants.HORIZONTAL);
-	JButton btnNew = new JButton(newAction);
-	btnNew.setIcon(new ImageIcon("icons/plus.png"));
+	 super(SwingConstants.HORIZONTAL);
+	
+	 setLayout(new BoxLayout(this,BoxLayout.X_AXIS));
+	
+	 add(Box.createHorizontalStrut(5));
+	
+	 JButton btnNew = new JButton(newAction);
+	 btnNew.setIcon(new ImageIcon("icons/plus.png"));
+	 btnNew.setPreferredSize(new Dimension(20,20));
+	 btnNew.setMinimumSize(new Dimension(20,20));
+	 btnNew.setMaximumSize(new Dimension(20,20));
+	 btnNew.setBorder(null);
+	
 	add(btnNew);
 
-	addSeparator();
+	 add(Box.createHorizontalStrut(10));
 
-	JButton btnEdit = new JButton();
-	btnEdit.setIcon(new ImageIcon("icons/pen.png"));
-	add(btnEdit);
+	 JButton btnEdit = new JButton(editAction);
+	 btnEdit.setIcon(new ImageIcon("icons/pen.png"));
+	 btnEdit.setPreferredSize(new Dimension(20,20));
+	 btnEdit.setMinimumSize(new Dimension(20,20));
+	 btnEdit.setMaximumSize(new Dimension(20,20));
+	 btnEdit.setBorder(null);
+   add(btnEdit);
+     add(Box.createHorizontalStrut(15));
 
-	addSeparator();
-
-	JButton btnDelete = new JButton();
-	btnDelete.setIcon(new ImageIcon("icons/trashbin.png"));
+	 JButton btnDelete = new JButton(deleteAction);
+	 btnDelete.setIcon(new ImageIcon("icons/trashbin.png"));
+	 btnDelete.setPreferredSize(new Dimension(20,20));
+	 btnDelete.setMinimumSize(new Dimension(20,20));
+	 btnDelete.setMaximumSize(new Dimension(20,20));
+	 btnDelete.setBorder(null);
 	add(btnDelete);
 	
+	 add(Box.createHorizontalGlue());
 	
+	 searchbar = new JTextField(20);
+	 searchbar.setPreferredSize(new Dimension(40,25));
+	 searchbar.setMinimumSize(new Dimension(40,25));
+	 searchbar.setMaximumSize(new Dimension(40,25));
+	add(searchbar); 
+	  
 	
-	setFloatable(true);
-	
-	
+	  JButton btnSearch = new JButton();
+	  btnSearch.setIcon(new ImageIcon("icons/search.png"));
+	  btnSearch.setPreferredSize(new Dimension(20,20));
+	  btnSearch.setMinimumSize(new Dimension(20,20));
+	  btnSearch.setMaximumSize(new Dimension(20,20));
+
+	 add(btnSearch);
+	  
+	  add(Box.createHorizontalStrut(15));
 	
 	}
 }
